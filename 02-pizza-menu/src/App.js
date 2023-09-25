e7782111b73b0c6489d6e43d95c1d2e55ee47d21
jijiezhou <jijiez@email.unc.edu>
2023-09-25 16:00:49 -0400

    npx create-react-app

---
+++ 02-pizza-menu/src/App.js
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-09-25 14:54:44
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-09-25 15:57:51
+ */
+import "./App.css";
+
+function App() {
+  return <h1>Hello React</h1>;
+}
+
+export default App;

4beb32705228c465a973adc1162b3aee659c22cd
jijiezhou <jijiez@email.unc.edu>
2023-09-25 17:37:23 -0400

    starer code of pizza-menu

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-09-25 17:20:01
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-09-25 17:31:19
+ */
+import React from "react";
+import ReacDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return <h1>Hello React</h1>;
+}
+
+//React v18: render root
+const root = ReacDOM.createRoot(document.getElementById("root"));
+//root.render(<App />);
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//React before 18
+//import React from "react";
+//import ReacDOM from "react-dom"; //不需要client
+//ReactDOM.render(<App />);

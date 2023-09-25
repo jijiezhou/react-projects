38945e7fba2f4ecddc80633605219f943d6e8614
jijiezhou <jijiez@email.unc.edu>
2023-09-25 18:08:18 -0400

    Javascript logic

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-09-25 17:20:01
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-09-25 17:48:19
+ * @LastEditTime: 2023-09-25 18:07:34
  */
 import React from "react";
 import ReacDOM from "react-dom/client";
@@ -57,12 +57,40 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Our Menu</h2>
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
   );
+}
+
+function Footer() {
+  const hour = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hour >= openHour && hour <= closeHour;
+
+  //   if (hour >= openHour && hour <= closeHour) alert("We're currently open");
+  //   else alert("Sorry we're closed");
+  return (
+    <footer>{new Date().toLocaleTimeString()}. We're currently open</footer>
+  );
+  //return React.createElement("footer", null, "We're currently open");
 }
 
 function Pizza() {

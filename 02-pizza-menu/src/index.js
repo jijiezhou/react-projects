da87f3cbf1be1da58c59d6f94eed92416b71f81b
jijiezhou <jijiez@email.unc.edu>
2023-09-25 21:39:20 -0400

    conditional rendering with &&

--- 02-pizza-menu/src/index.js
+++ 02-pizza-menu/src/index.js
@@ -4,7 +4,7 @@
  * @Author: ZJJ
  * @Date: 2023-09-25 17:20:01
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-09-25 21:22:19
+ * @LastEditTime: 2023-09-25 21:38:11
  */
 import React from "react";
 import ReacDOM from "react-dom/client";
@@ -76,14 +76,20 @@ function Header() {
 }
 
 function Menu() {
+  const pizzas = pizzaData;
+  const numPizzas = pizzas.length;
+
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <ul className="pizzas">
-        {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
-        ))}
-      </ul>
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza) => (
+            <Pizza pizzaObj={pizza} key={pizza.name} />
+          ))}
+        </ul>
+      )}
+
       {/*
       <Pizza
         name="Pizza Spinaci"
@@ -124,7 +130,14 @@ function Footer() {
   //   else alert("Sorry we're closed");
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. We're currently open
+      {isOpen && (
+        <div className="order">
+          <p>
+            We're open untill {closeHour}:00. Come visit us or order online.
+          </p>
+          <button className="btn">Order</button>
+        </div>
+      )}
     </footer>
   );
   //return React.createElement("footer", null, "We're currently open");

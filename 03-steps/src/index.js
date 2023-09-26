c20cb8afb7e2baf9fb32a14c8b94c039fa0a838c
jijiezhou <jijiez@email.unc.edu>
2023-09-25 23:39:05 -0400

    steps

---
+++ 03-steps/src/index.js
@@ -0,0 +1,19 @@
+/*
+ * @Descripttion: ZJJ Code
+ * @version: 1.0.0
+ * @Author: ZJJ
+ * @Date: 2023-09-25 23:19:15
+ * @LastEditors: ZJJ
+ * @LastEditTime: 2023-09-25 23:25:04
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

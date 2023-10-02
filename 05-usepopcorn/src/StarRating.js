a87b30f3faaa19512623ebec3095e4a2bd8ae1d3
jijiezhou <jijiez@email.unc.edu>
2023-10-02 01:32:03 -0400

    handling hover events

--- 05-usepopcorn/src/StarRating.js
+++ 05-usepopcorn/src/StarRating.js
@@ -6,7 +6,7 @@ import { useState } from "react";
  * @Author: ZJJ
  * @Date: 2023-10-02 00:53:44
  * @LastEditors: ZJJ
- * @LastEditTime: 2023-10-02 01:20:28
+ * @LastEditTime: 2023-10-02 01:29:50
  */
 const containerStyle = {
   display: "flex",
@@ -25,6 +25,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -37,11 +38,13 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             key={i}
             onRate={() => handleRating(i + 1)}
-            full={rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
       </div>
-      <p style={textStyle}>{rating || ""}</p>
+      <p style={textStyle}>{tempRating || rating || ""}</p>
     </div>
   );
 }
@@ -53,9 +56,15 @@ const starStyle = {
   cursor: "pointer",
 };
 
-function Star({ onRate, full }) {
+function Star({ onRate, full, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {full ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
